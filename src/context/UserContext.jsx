f4da8994316298f90fecc64f33d0398e452849d3
YPachado99 <jonathanpachado73@gmail.com>
2024-07-10 22:19:17 -0300

    fix usercontext y productocontext

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,126 +1,100 @@
 import axios from "axios";
 import jwt_decode from "jwt-decode";
-import Swal from "sweetalert2";
 import { createContext, useEffect, useState } from "react";
 
 export const UsuariosContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 const UserContext = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [userLogueado, setUserLogueado] = useState(null);
+  const [users, setUsers] = useState();
+  const [userLogueado, setUserLogueado] = useState();
 
   const getUsers = async () => {
     try {
       const response = await axios.get(
         "https://back-codestockers.vercel.app/api/user/usuarios"
       );
+      console.log(response);
       setUsers(response.data);
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   };
 
   const login = async (mail, contraseña) => {
-    try {
-      const response = await axios.post(
-        "https://back-codestockers.vercel.app/api/user/login",
-        { mail, contraseña }
-      );
+    //console.log(mail, contraseña, "login Context");
+    const response = await axios.post("https://back-codestockers.vercel.app/api/user/login", {
+      mail,
+      contraseña,
+    });
+    //console.log(response);
 
-      const jwtToken = response.data.data.token;
-      const jwtDecode = jwt_decode(jwtToken);
+    const jwtToken = response.data.data.token;
+    const jwtDecode = jwt_decode(jwtToken);
+    console.log(jwtDecode);
 
-      const user = {
-        id: jwtDecode.id,
-        nombre: jwtDecode.nombre,
-        apellido: jwtDecode.apellido,
-        mail: jwtDecode.mail,
-        role: jwtDecode.role,
-        telefono: jwtDecode.telefono,
-        url: jwtDecode.url,
-        usuarioAdm: jwtDecode.usuarioAdm,
-      };
+    //setUserLogueado(jwtDecode)
 
-      localStorage.setItem("user", JSON.stringify(user));
-      setUserLogueado(user);
+    const user = {
+      id: jwtDecode.id,
+      nombre: jwtDecode.nombre,
+      apellido: jwtDecode.apellido,
+      mail: jwtDecode.mail,
+      role: jwtDecode.role,
+      telefono: jwtDecode.telefono,
+      url: jwtDecode.url,
+      usuarioAdm: jwtDecode.usuarioAdm,
+    };
 
+    localStorage.setItem("user", JSON.stringify(user));
+    setUserLogueado(user);
+    //console.log(user);
+
+    if (user.role === "admin") {
       window.location.href = "/home";
-    } catch (error) {
-      console.error(error);
-      Swal.fire({
-        icon: "error",
-        title: "Error en el inicio de sesión",
-        text: "Usuario y/o contraseña incorrecto",
-      });
+    } else {
+      window.location.href = "/home";
     }
   };
 
   const postUsuario = async (user) => {
     try {
-      const response = await axios.post(
+      let response = await axios.post(
         "https://back-codestockers.vercel.app/api/user/register",
         user
       );
+      console.log(response);
 
-      setUsers((prevUsers) => [...prevUsers, response.data]);
-      Swal.fire({
-        icon: "success",
-        title: "Usuario creado",
-        text: "El usuario ha sido creado exitosamente.",
-      });
+      setProductos([...users, response.data]);
+      window.location.reload();
     } catch (error) {
-      console.error(error);
-      Swal.fire({
-        icon: "error",
-        title: "Error al crear usuario",
-        text: "Hubo un problema al crear el usuario. Inténtalo nuevamente.",
-      });
+      console.log(error);
     }
   };
-
   const deleteUsuario = async (_id) => {
+    //console.log(_id, "deleteUsuario");
     try {
       await axios.delete(`https://back-codestockers.vercel.app/api/user/usuarios/${_id}`);
-      setUsers((prevUsers) => prevUsers.filter((user) => user._id !== _id));
-      Swal.fire({
-        icon: "success",
-        title: "Usuario eliminado",
-        text: "El usuario ha sido eliminado exitosamente.",
-      });
+      window.location.reload();
     } catch (error) {
-      console.error(error);
-      Swal.fire({
-        icon: "error",
-        title: "Error al eliminar usuario",
-        text: "Hubo un problema al eliminar el usuario. Inténtalo nuevamente.",
-      });
+      console.log(error);
     }
   };
 
   const updateUser = async (updatedUsuario) => {
+    //console.log(updatedUsuario, "este es el user");
     try {
       await axios.put(
         `https://back-codestockers.vercel.app/user/usuarios/${updatedUsuario._id}`,
         updatedUsuario
       );
-      setUsers((prevUsers) =>
-        prevUsers.map((user) =>
-          user._id === updatedUsuario._id ? updatedUsuario : user
-        )
+      const newUsuarios = usuario.map((usuario) =>
+        usuario._id === updatedUsuario._id ? updatedUsuario : usuario
       );
-      Swal.fire({
-        icon: "success",
-        title: "Usuario actualizado",
-        text: "El usuario ha sido actualizado exitosamente.",
-      });
+      setUsers(newUsuarios);
+      window.location.reload();
     } catch (error) {
-      console.error(error);
-      Swal.fire({
-        icon: "error",
-        title: "Error al actualizar usuario",
-        text: "Hubo un problema al actualizar el usuario. Inténtalo nuevamente.",
-      });
+      console.log(error);
     }
   };
 
@@ -152,4 +126,3 @@ const UserContext = ({ children }) => {
 };
 
 export default UserContext;
-
