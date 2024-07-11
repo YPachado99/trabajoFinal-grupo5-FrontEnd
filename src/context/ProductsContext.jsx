adbbd1296284be1f846a6c1084a5129073b00406
YPachado99 <jonathanpachado73@gmail.com>
2024-07-10 21:44:51 -0300

    quitando console.log

--- src/context/ProductsContext.jsx
+++ src/context/ProductsContext.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+/*import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
 export const ProductosContext = createContext();
@@ -62,6 +62,113 @@ const ProductsContext = ({ children }) => {
   }, []);
 
   //console.log(productos, "---desde context");
+  return (
+    <ProductosContext.Provider
+      value={{
+        productos,
+        setProductos,
+        postProducto,
+        deleteProducto,
+        updateProducto,
+      }}
+    >
+      {children}
+    </ProductosContext.Provider>
+  );
+};
+
+export default ProductsContext;*/
+
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import Swal from "sweetalert2";
+
+export const ProductosContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+const ProductsContext = ({ children }) => {
+  const [productos, setProductos] = useState([]);
+
+  const getProductos = async () => {
+    try {
+      const response = await axios.get("https://back-codestockers.vercel.app/api/productos");
+      setProductos(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const postProducto = async (producto) => {
+    try {
+      const response = await axios.post(
+        "https://back-codestockers.vercel.app/api/productos",
+        producto
+      );
+      setProductos((prevProductos) => [...prevProductos, response.data]);
+      Swal.fire({
+        icon: "success",
+        title: "Producto creado",
+        text: "El producto ha sido creado exitosamente.",
+      });
+    } catch (error) {
+      console.error(error);
+      Swal.fire({
+        icon: "error",
+        title: "Error al crear producto",
+        text: "Hubo un problema al crear el producto. Inténtalo nuevamente.",
+      });
+    }
+  };
+
+  const deleteProducto = async (_id) => {
+    try {
+      await axios.delete(`https://back-codestockers.vercel.app/api/productos/${_id}`);
+      setProductos((prevProductos) => prevProductos.filter((producto) => producto._id !== _id));
+      Swal.fire({
+        icon: "success",
+        title: "Producto eliminado",
+        text: "El producto ha sido eliminado exitosamente.",
+      });
+    } catch (error) {
+      console.error(error);
+      Swal.fire({
+        icon: "error",
+        title: "Error al eliminar producto",
+        text: "Hubo un problema al eliminar el producto. Inténtalo nuevamente.",
+      });
+    }
+  };
+
+  const updateProducto = async (updatedProducto) => {
+    try {
+      await axios.put(
+        `https://back-codestockers.vercel.app/api/productos/${updatedProducto._id}`,
+        updatedProducto
+      );
+      setProductos((prevProductos) =>
+        prevProductos.map((producto) =>
+          producto._id === updatedProducto._id ? updatedProducto : producto
+        )
+      );
+      Swal.fire({
+        icon: "success",
+        title: "Producto actualizado",
+        text: "El producto ha sido actualizado exitosamente.",
+      });
+    } catch (error) {
+      console.error(error);
+      Swal.fire({
+        icon: "error",
+        title: "Error al actualizar producto",
+        text: "Hubo un problema al actualizar el producto. Inténtalo nuevamente.",
+      });
+    }
+  };
+
+  useEffect(() => {
+    getProductos();
+  }, []);
+
   return (
     <ProductosContext.Provider
       value={{
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+/*import axios from "axios";
 import jwt_decode from "jwt-decode";
 import { createContext, useEffect, useState } from "react";
 
@@ -27,7 +27,7 @@ const UserContext = ({ children }) => {
       mail,
       contraseña,
     });
-    console.log(response);
+    //console.log(response);
 
     const jwtToken = response.data.data.token;
     const jwtDecode = jwt_decode(jwtToken);
@@ -125,4 +125,160 @@ const UserContext = ({ children }) => {
   );
 };
 
+export default UserContext;*/
+
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import Swal from "sweetalert2";
+import { createContext, useEffect, useState } from "react";
+
+export const UsuariosContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+const UserContext = ({ children }) => {
+  const [users, setUsers] = useState([]);
+  const [userLogueado, setUserLogueado] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get(
+        "https://back-codestockers.vercel.app/api/user/usuarios"
+      );
+      setUsers(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const login = async (mail, contraseña) => {
+    try {
+      const response = await axios.post(
+        "https://back-codestockers.vercel.app/api/user/login",
+        { mail, contraseña }
+      );
+
+      const jwtToken = response.data.data.token;
+      const jwtDecode = jwt_decode(jwtToken);
+
+      const user = {
+        id: jwtDecode.id,
+        nombre: jwtDecode.nombre,
+        apellido: jwtDecode.apellido,
+        mail: jwtDecode.mail,
+        role: jwtDecode.role,
+        telefono: jwtDecode.telefono,
+        url: jwtDecode.url,
+        usuarioAdm: jwtDecode.usuarioAdm,
+      };
+
+      localStorage.setItem("user", JSON.stringify(user));
+      setUserLogueado(user);
+
+      window.location.href = "/home";
+    } catch (error) {
+      console.error(error);
+      Swal.fire({
+        icon: "error",
+        title: "Error en el inicio de sesión",
+        text: "Usuario y/o contraseña incorrecto",
+      });
+    }
+  };
+
+  const postUsuario = async (user) => {
+    try {
+      const response = await axios.post(
+        "https://back-codestockers.vercel.app/api/user/register",
+        user
+      );
+
+      setUsers((prevUsers) => [...prevUsers, response.data]);
+      Swal.fire({
+        icon: "success",
+        title: "Usuario creado",
+        text: "El usuario ha sido creado exitosamente.",
+      });
+    } catch (error) {
+      console.error(error);
+      Swal.fire({
+        icon: "error",
+        title: "Error al crear usuario",
+        text: "Hubo un problema al crear el usuario. Inténtalo nuevamente.",
+      });
+    }
+  };
+
+  const deleteUsuario = async (_id) => {
+    try {
+      await axios.delete(`https://back-codestockers.vercel.app/api/user/usuarios/${_id}`);
+      setUsers((prevUsers) => prevUsers.filter((user) => user._id !== _id));
+      Swal.fire({
+        icon: "success",
+        title: "Usuario eliminado",
+        text: "El usuario ha sido eliminado exitosamente.",
+      });
+    } catch (error) {
+      console.error(error);
+      Swal.fire({
+        icon: "error",
+        title: "Error al eliminar usuario",
+        text: "Hubo un problema al eliminar el usuario. Inténtalo nuevamente.",
+      });
+    }
+  };
+
+  const updateUser = async (updatedUsuario) => {
+    try {
+      await axios.put(
+        `https://back-codestockers.vercel.app/user/usuarios/${updatedUsuario._id}`,
+        updatedUsuario
+      );
+      setUsers((prevUsers) =>
+        prevUsers.map((user) =>
+          user._id === updatedUsuario._id ? updatedUsuario : user
+        )
+      );
+      Swal.fire({
+        icon: "success",
+        title: "Usuario actualizado",
+        text: "El usuario ha sido actualizado exitosamente.",
+      });
+    } catch (error) {
+      console.error(error);
+      Swal.fire({
+        icon: "error",
+        title: "Error al actualizar usuario",
+        text: "Hubo un problema al actualizar el usuario. Inténtalo nuevamente.",
+      });
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    window.location.href = "/";
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <UsuariosContext.Provider
+      value={{
+        users,
+        setUsers,
+        logout,
+        deleteUsuario,
+        postUsuario,
+        updateUser,
+        login,
+        userLogueado,
+      }}
+    >
+      {children}
+    </UsuariosContext.Provider>
+  );
+};
+
 export default UserContext;
+
